8cb442aa4d870b8355cae132ff4467abdf47163f
Caleb <calebbelcourt@hotmail.com>
2021-05-12 12:30:23 -0700

    guess hard coded word
    the player can guess the hard coded 4 letter word

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,22 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Hello there. Welcome to the Bull Cows Game"));
+    PrintLine(TEXT("Please guess the 4 letter word."));
+    PrintLine(TEXT(""));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    FString HiddenWord = TEXT("bread");
 
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("Hey, great job!"));
+    }
+    else
+    {
+        PrintLine(TEXT("Incorrect! Try again."));
+    }
 }
